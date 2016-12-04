a5f2807d4b2f96a43d610c953efd83e4fe55e87d
Robert MacGregor <ragoradev@gmail.com>
2016-12-04 14:27:19 -0500

    Convert pesky tabs to spaces

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 """
-	main.py
+    main.py
 
-	This software is licensed under the MIT license. See LICENSE for more information.
+    This software is licensed under the MIT license. See LICENSE for more information.
 """
 
 import re
@@ -12,28 +12,28 @@ import struct
 import exportmission
 
 class Application(object):
-	signatures = {
-		"// Mission file created by ConvertMission from": exportmission.process_mission,
-	}
+    signatures = {
+        "// Mission file created by ConvertMission from": exportmission.process_mission,
+    }
 
-	def main(self):
-		with open("Tribes - Aerial Assault.bin", "r") as handle:
-			buffer = handle.read()
-			buffer_length = len(buffer)
+    def main(self):
+        with open("Tribes - Aerial Assault.bin", "r") as handle:
+            buffer = handle.read()
+            buffer_length = len(buffer)
 
-			print("Opened file. Length: %u" % buffer_length)
-			for signature in self.signatures:
-				signature_pattern = re.compile(signature)
-				print("Searching for signature - %s" % repr(signature))
+            print("Opened file. Length: %u" % buffer_length)
+            for signature in self.signatures:
+                signature_pattern = re.compile(signature)
+                print("Searching for signature - %s" % repr(signature))
 
-				signature_length = len(signature)
+                signature_length = len(signature)
 
-				for match in signature_pattern.finditer(buffer):
-					match_text = match.group(0)
-					match_start = match.start()
+                for match in signature_pattern.finditer(buffer):
+                    match_text = match.group(0)
+                    match_start = match.start()
 
-					tested_buffer = buffer[match_start:match_start + signature_length]
-					self.signatures[signature](self, signature, buffer, match_start)
+                    tested_buffer = buffer[match_start:match_start + signature_length]
+                    self.signatures[signature](self, signature, buffer, match_start)
 
 if __name__ == "__main__":
-	Application().main()
+    Application().main()
